d2df6f0d3e1f79717af6702bdb7c67595b48ff43
ivanhrytsyna <ivangritsina@gmail.com>
2024-04-23 16:24:14 +0300

    .

--- home_page/page_objects/full_screen_page_objects.py
+++ home_page/page_objects/full_screen_page_objects.py
@@ -2,6 +2,7 @@ import os
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 
+
 class PageObject:
     def __init__(self, driver):
         self.driver = driver
@@ -16,9 +17,6 @@ class PageObject:
 
         width = self.driver.execute_script("return document.body.scrollWidth")
         height = self.driver.execute_script("return document.body.scrollHeight")
-        # s = lambda x: self.driver.execute_script('return document.body.parentNode.scroll' + x)
-        # self.driver.set_window_size(s('Width'), s('Height'))
-        # self.driver.find_element(By.TAG_NAME, 'body')
 
         self.driver.set_window_size(width, height)
 
